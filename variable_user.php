bc6da93a3d7046f7a92d3cdff404040f7fc57272
Bobogeng <irsalfathifarhat@gmail.com>
2024-06-14 13:31:01 +0700

    feat: finished pertemuan-1

---
+++ variable_user.php
@@ -0,0 +1,10 @@
+<?php
+$nama = 'Irsal Fathi Farhat'; // Tipe Data String
+$umur = 20; // Tipe Data Integer
+$berat = 45.5; // Tipe Data Float/Double
+
+echo 'Nama : $nama' . $nama;
+echo '</br>Umur : ' . $umur . ' Tahun';
+echo '</br>Berat : ' . $berat . ' Kg';
+
+echo "</br>Hello $nama Apakabar";
